1b4d8a4c1cad89582a7abd09109c818b4d60c8e4
Beimar Choque Mamani <the.bboy.bb@gmail.com>
2025-05-25 14:19:29 -0400

    changes in the stub service FINAL

--- Final.test/stub/StudentServiceStub.cs
+++ Final.test/stub/StudentServiceStub.cs
@@ -5,15 +5,19 @@ namespace Final.test.stub
 {
     public class StudentServiceStub : IStudentService
     {
-        public bool hasapproved(Estudiante estudiante)
+     public bool hasapproved(Estudiante estudiante)
         {
-            if (estudiante.Nota >= 51)
+            if (estudiante.Nota >= 51 && estudiante.Nota <= 100)
             {
                 return true;
             }
-            else
+            else if (estudiante.Nota >= 0 && estudiante.Nota < 51)
             {
                 return false;
+            }
+            else
+            {
+                throw new ArgumentException("La nota del estudiante debe estar entre 0 y 100.");
             }
         }
     }
